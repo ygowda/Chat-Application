3c2896d1b26042c91943e1b7c14636875590c46d
ygowda <gowdayash@yahoo.com>
2017-09-15 22:30:01 -0700

    username and post functionality

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 var app = require('express')();
+var bodyParser = require('body-parser');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var express = require('express');
 var emoji = require('node-emoji')
 
 app.use(express.static(__dirname + '/images'));
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+var username; 
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/welcome_page.html');
+});
+
+app.post('/homepage', function(req, res){
+
+  username = req.body.user.name;
+
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
 
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  	// socket.emit('news', {hello: 'world'});
+
+    if(!username){
+      username = "me";
+    }
+    socket.emit('chat message', username +": " +msg);
   });
 
   socket.on('disconnect', function(msg){
-    io.emit('user disonnected', msg);
+    socket.emit('user disonnected', msg);
   });
+
 });
 
 
